b81210946a1d37283cbcdbc979a936dedde5aebb
sskobyak <scob@ya.ru>
2026-03-27 10:20:32 +0100

    icons

--- front/src/components/layouts/MainLayout.jsx
+++ front/src/components/layouts/MainLayout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import { useNavigate } from "react-router";
-
+import { IoMdExit } from "react-icons/io";
 
 export default function AuthLayout() {
   const navigate = useNavigate()
@@ -12,7 +12,7 @@ export default function AuthLayout() {
 
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <div className="absolute top-2 right-2 cursor-pointer" onClick={()=>onLogout()}>Logout</div>
+        <div className="absolute top-2 right-2 cursor-pointer" onClick={()=>onLogout()}><IoMdExit size={20}/></div>
       <Outlet />
     </div>
   );
