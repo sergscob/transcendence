9d6f17822f89c778c69da0bc7e919e457fb85eed
sskobyak <scob@ya.ru>
2026-03-27 12:46:26 +0100

    tester

--- back/core/settings.py
+++ back/core/settings.py
@@ -133,7 +133,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60000),
     'AUTH_HEADER_TYPES': ('Bearer',),
 }
 
