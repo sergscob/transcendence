73b96455c11eedf9acf7f1b9051c1aef30253071
sskobyak <scob@ya.ru>
2026-03-30 11:58:08 +0200

    login 42

--- back/users/urls.py
+++ back/users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView
 from .views_google import google_login, google_callback
+from .views_e42 import e42_login, e42_callback
 from .views import RegisterView, ProfileView
 from .views_profile import ProfileUpdateView
 
@@ -10,6 +11,8 @@ urlpatterns = [
 
     path("api/auth/google/", google_login),
     path("api/auth/google/callback/", google_callback),
+    path("api/auth/e42/", e42_login),
+    path("api/auth/e42callback/", e42_callback),
 
     path('api/profile/', ProfileView.as_view()),
     path('api/profile/update/', ProfileUpdateView.as_view()),
---
+++ back/users/views_e42.py
@@ -0,0 +1,56 @@
+from django.conf import settings
+from django.shortcuts import redirect
+import requests
+from django.contrib.auth import get_user_model
+from rest_framework_simplejwt.tokens import RefreshToken
+
+def e42_login(request):
+    url = (
+        "https://api.intra.42.fr/oauth/authorize"
+        f"?client_id={settings.E42_CLIENT_ID}"
+        f"&redirect_uri={settings.E42_REDIRECT_URI}"
+        "&response_type=code"
+    )
+    return redirect(url)
+
+
+User = get_user_model()
+
+def e42_callback(request):
+    code = request.GET.get("code")
+
+    token_res = requests.post(
+        "https://api.intra.42.fr/oauth/token",
+        data={
+            "grant_type": "authorization_code",
+            "client_id": settings.E42_CLIENT_ID,
+            "client_secret": settings.E42_CLIENT_SECRET,
+            "code": code,
+            "redirect_uri": settings.E42_REDIRECT_URI,
+        },
+    )
+
+    if (token_res.status_code != 200): print("E42 RESPONSE:", token_res.text)
+    token_res = token_res.json()    
+
+    access_token = token_res.get("access_token")
+
+    user_info = requests.get(
+        "https://api.intra.42.fr/v2/me",
+        headers={"Authorization": f"Bearer {access_token}"}
+    ).json()
+
+    if (user_info is None): print("Me is none")
+
+    username = f"{user_info.get("usual_full_name")} (42 login: {user_info.get("login")})"
+    email = user_info.get("email")
+    # 'email': 'sskobyak@student.42.fr', 'login': 'sskobyak', 'first_name': 'Sergey', 'last_name': 'Skobyakov', 'usual_full_name': 'Sergey Skobyakov'
+
+    user, _ = User.objects.get_or_create(
+         email=email,
+         defaults={"username": username}
+    )
+
+    refresh = RefreshToken.for_user(user)
+
+    return redirect(f"{settings.FRONTEND_URL}/oauth?token={refresh.access_token}")
