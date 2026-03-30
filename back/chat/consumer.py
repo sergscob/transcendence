049b385ad03f2f1c2d8918e6d25fde86071a362c
sskobyak <scob@ya.ru>
2026-03-30 23:53:09 +0200

    async channel

--- back/chat/consumer.py
+++ back/chat/consumer.py
@@ -1,31 +1,36 @@
-from channels.generic.websocket import JsonWebsocketConsumer
+from channels.generic.websocket import AsyncJsonWebsocketConsumer 
 
-class ChatConsumer(JsonWebsocketConsumer):
-    def connect(self):
+class ChatConsumer(AsyncJsonWebsocketConsumer ):
+    print ("ChatConsumer initialized")
+    async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = f'chat_{self.room_name}'
 
+        print ("Connecting to room:", self.room_name)
+        # raise Exception("Test") 
         # Join room group
-        self.channel_layer.group_add(
+        await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
         )
 
-        self.accept()
+        await self.accept()
 
-    def disconnect(self, close_code):
+    async def disconnect(self, close_code):
         # Leave room group
-        self.channel_layer.group_discard(
+        await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
 
     # Receive message from WebSocket (already parsed as dict)
-    def receive_json(self, content, **kwargs):
+    async def receive_json(self, content, **kwargs):
         message = content['message']
 
+        print ("Received message:", message)
+
         # Send message to room group
-        self.channel_layer.group_send(
+        await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'chat_message',
@@ -34,10 +39,12 @@ class ChatConsumer(JsonWebsocketConsumer):
         )
 
     # Receive message from room group
-    def chat_message(self, event):
+    async def chat_message(self, event):
+        print ("Chat message:", event['message'])
+
         message = event['message']
 
         # Send message to WebSocket (as JSON)
-        self.send_json({
+        await self.send_json({
             'message': message
         })
