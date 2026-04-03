b914d1062fda4df336593d9d6346e04233d5a4ab
sskobyak <scob@ya.ru>
2026-04-03 11:02:20 +0200

    menu

--- front/src/components/layouts/MainLayout.jsx
+++ front/src/components/layouts/MainLayout.jsx
@@ -1,15 +1,17 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
 import { useNavigate } from "react-router";
-import { IoMdExit } from "react-icons/io";
 import { useUserStore } from "@/stores/userStore";
 import { useEffect } from "react";
+
+import { IoMdExit } from "react-icons/io";
+import IconMenu from "@/assets/icons/menu.svg?react";
+
 
 export default function MainLayout() {
   const navigate = useNavigate()
   const loadUser = useUserStore((s) => s.loadUser);
   const user = useUserStore((s) => s.user);
   useEffect(() => {
-    console.log(111)
     loadUser(); 
   }, []);
 
@@ -20,6 +22,11 @@ export default function MainLayout() {
 
   return (
     <div className="min-h-screen flex items-center justify-center">
+      <div>
+        <Link to="/"> 
+          <IconMenu className="absolute top-2 left-2 mb-4 h-6 w-6 text-slate-900"/>
+        </Link>
+      </div>
       <div className="absolute top-2 right-2 cursor-pointer" onClick={()=>onLogout()}>
         {
           user 
