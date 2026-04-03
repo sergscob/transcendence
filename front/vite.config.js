bc1b81a50cf37ec512ef50928759e0754659ccff
sskobyak <scob@ya.ru>
2026-04-03 11:02:35 +0200

    game

--- front/vite.config.js
+++ front/vite.config.js
@@ -2,11 +2,14 @@ import { defineConfig } from 'vite'
 import path from 'path'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import svgr from 'vite-plugin-svgr'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    svgr(),
+
     tailwindcss(),
   ],
   resolve: {
