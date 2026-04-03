9c3680acb2a3693f47b003ffff849bcc95f265c8
sskobyak <scob@ya.ru>
2026-04-03 17:13:25 +0200

    mainmenu

---
+++ front/src/components/ui_int/BigButton.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+export default function Index({text, url, className}) {
+  return (
+    <Link className="simple-link" to={url}>
+        <div className={
+            `w-full text-white font-bold text-center py-4 px-14 border text-lg rounded-3xl hover:transition hover:duration-300 shadow-xl 
+             ${className}
+            ` } >
+            {text}
+        </div>
+    </Link>
+  );
+}
