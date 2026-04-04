5b146d170e58c62b21891b741d757eb787d72460
sskobyak <scob@ya.ru>
2026-04-04 13:08:27 +0200

    chat refactoring

--- back/sockets/game.py
+++ back/sockets/game.py
@@ -2,26 +2,13 @@ from channels.generic.websocket import AsyncJsonWebsocketConsumer
 from django.conf import settings
 from collections import deque
 
-from .bot import load_model, generate_bot_response
-import asyncio
+ROOM_GAME_STATE = {}
 
-ROOM_CHAT_HISTORY = {}
-
-def get_room_history(room_name):
-    history = ROOM_CHAT_HISTORY.get(room_name)
-    if history is None:
-        history = deque(maxlen=getattr(settings, 'CHAT_MEMORY_LIMIT', 100))
-        ROOM_CHAT_HISTORY[room_name] = history
-    return history
-
-
-class ChatConsumer(AsyncJsonWebsocketConsumer ):
-    print ("ChatConsumer initialized")
+class PlayerConsumer(AsyncJsonWebsocketConsumer ):
 
     async def connect(self):
-        load_model()  
         self.room_name = self.scope['url_route']['kwargs']['room_name']
-        self.room_group_name = f'chat_{self.room_name}'
+        self.room_group_name = f'game_{self.room_name}'
 
         self.chat_history_ids = None
 
@@ -34,10 +21,9 @@ class ChatConsumer(AsyncJsonWebsocketConsumer ):
         )
 
         await self.accept()
-        await self.send_room_history()
+        await self.send_room_state()
 
     async def disconnect(self, close_code):
-        # Leave room group
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
@@ -45,55 +31,31 @@ class ChatConsumer(AsyncJsonWebsocketConsumer ):
 
     async def receive_json(self, content, **kwargs):
         message = content['message']
-        username = content['username']
         user_id = content['user_id']
 
         print("Received message:", message)
 
         user_message_event = {
-            'type': 'chat_message',
+            'type': 'game_message',
             'message': message,
-            'username': username,
             'user_id': user_id,
         }
-        self.append_to_history(user_message_event)
+        self.update_game_state(user_message_event)
 
         await self.channel_layer.group_send(
             self.room_group_name,
             user_message_event
         )
 
-        # BOT
-        bot_response, self.chat_history_ids = await asyncio.to_thread(
-            generate_bot_response,
-            self.chat_history_ids,
-            message,
-        )
-        print("Bot response:", bot_response)
-
-        bot_message_event = {
-            'type': 'chat_message',
-            'message': bot_response,
-            'username': 'Bot',
-            'user_id': 0,
-        }
-        self.append_to_history(bot_message_event)
-
-        await self.channel_layer.group_send(
-            self.room_group_name,
-            bot_message_event
-        )
-
-    async def chat_message(self, event):
-        print ("Chat message:", event['message'])
+    async def game_message(self, event):
+        print ("Game message:", event['message'])
 
         await self.send_json({
             'message': event['message'],
-            'username': event['username'],
             'user_id': event['user_id']
         })
 
-    async def send_room_history(self):
+    async def send_room_state(self):
         room_messages = list(get_room_history(self.room_name))
         for item in room_messages:
             await self.send_json({
--- back/sockets/routing.py
+++ back/sockets/routing.py
@@ -1,8 +1,8 @@
 from django.urls import re_path
-from . import consumer
+from . import chat
 
 websocket_urlpatterns = [
-    re_path(r'^ws/chat/(?P<room_name>\w+)/$', consumer.ChatConsumer.as_asgi()),
+    re_path(r'^ws/chat/(?P<room_name>\w+)/$', chat.ChatConsumer.as_asgi()),
     # re_path(r'^.*$', consumer.ChatConsumer.as_asgi()),
     
     # re_path(r'^ws/chat/(?P<room_name>\w+)/$', consumer.ChatConsumer.as_asgi()),
