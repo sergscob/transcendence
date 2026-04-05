429628149f71dafe45bc6439a528c4977816bac1
Irina Kalinina <irakalina2004@gmail.com>
2026-04-05 09:15:55 +0200

    EditFriends improuve

--- front/src/pages/EditFriends.jsx
+++ front/src/pages/EditFriends.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import API from "../api/api";
 import { useUserStore } from "@/stores/userStore";
 import FriendInfo from "../components/ui_int/FriendInfo";
+import addFriendIcon from "../assets/icons/addFriend.svg";
 
 function EditFriends() {
   const user = useUserStore((s) => s.user);
@@ -43,58 +44,68 @@ function EditFriends() {
   if (!user) return (<>loading</>)
 
   return (
-    <div className="w-screen h-screen flex justify-center items-center bg-gray-100">
-      <div>
-        <h2 className="text-lg font-bold">All Friends:</h2>
-        <ul>
-          {allFriends.map(friend => (
-            <li key={friend.id}>
-              <FriendInfo friend={friend} className="my-2">
-                {friend.accepted ? <span className="text-xs text-red-600"> ERROR! accepted</span> : ''}
-                {friend.invitation_sent ? 
-                    <span>
-                      <span className="text-xs text-green-600"> invitation sent </span> 
-                      <a className="text-xs text-red-400" onClick={() => deleteInvitation(friend.id)}> 
-                        delete invitation
-                      </a>
-                    </span>
-                  : <a className="pl-2 text-xs text-amber-800" onClick={() => sendInvitation(friend.id)}> 
-                      send request
-                    </a>
-                }
-              </FriendInfo>
-            </li>
-          ))}
-        </ul>
-        <h2 className="text-lg font-bold">Friends:</h2>
-        {friends.length ? 
-          <ul>
-            {friends.map(friend => (
-              <li key={friend.id}>
-                <FriendInfo friend={friend} className="my-2">
-                    <a className="text-xs text-red-400 pl-2" onClick={() => deleteInvitation(friend.id)}> 
-                        delete friend
-                    </a>
-                  </FriendInfo>
-              </li>
-            ))}
-          </ul>
-          : <div>No friends</div>}
-        <h2 className="text-lg font-bold">Waiting for Approval:</h2>
-        {waitingList.length ?  
-          <ul>
-            {waitingList.map(friend => (
-              <li key={friend.id}>
-                <FriendInfo friend={friend} className="my-2">
-                  <a className="text-xs text-green-600" onClick={() => acceptInvitation(friend.id)}>
-                    accept invitation
-                  </a>
+    <div className="w-screen h-screen flex justify-center items-center bg-fuchsia-200">
+      <div className="flex gap-30">
+
+        <div className="flex flex-col gap-4 border border-black rounded-md p-4 shadow-lg bg-white">
+            <h2 className="mr-10 text-lg font-bold text-[40px]">All Users:</h2>
+            <input
+                className="placeholder:text-gray-500 placeholder:italic rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                placeholder="Search for a user..." type="text" name="search"
+            />
+            <ul className="overscroll-auto max-h-64 overflow-y-auto p-4 space-y-8 border border-gray-300 rounded-md">
+            {allFriends.map(friend => (
+                <li key={friend.id}>
+                <FriendInfo friend={friend} className="my-2 text-[20px] gap-3 border-b border-gray-300 pb-2">
+                    {friend.accepted ? <span className="text-xs text-red-600"> ERROR! accepted</span> : ''}
+                    {friend.invitation_sent ?
+                        <span>
+                        <span className="text-xs text-green-600"> invitation sent </span>
+                        <a className="text-xs text-red-400" onClick={() => deleteInvitation(friend.id)}>
+                            delete invitation
+                        </a>
+                        </span>
+                    : <a title="Send friend request" onClick={() => sendInvitation(friend.id)}>
+                        <img src={addFriendIcon} alt="Send friend request" width="30" height="30"/>
+                        </a>
+                    }
                 </FriendInfo>
-              </li>
+                </li>
             ))}
-          </ul>
-          : <div>Nobody wants to be your friend</div>
-        }
+            </ul>
+        </div>
+
+        <div className="flex flex-col gap-4 border border-black rounded-md p-4 shadow-lg bg-white">
+            <h2 className="text-lg text-[40px] font-bold">Friends:</h2>
+            {friends.length ?
+            <ul>
+                {friends.map(friend => (
+                <li key={friend.id}>
+                    <FriendInfo friend={friend} className="my-2">
+                        <a className="text-xs text-red-400 pl-2" onClick={() => deleteInvitation(friend.id)}>
+                            delete friend
+                        </a>
+                    </FriendInfo>
+                </li>
+                ))}
+            </ul>
+            : <div>Add some friends from from All Users !</div>}
+            <h2 className="text-lg font-bold">Waiting for Approval:</h2>
+            {waitingList.length ?
+            <ul>
+                {waitingList.map(friend => (
+                <li key={friend.id}>
+                    <FriendInfo friend={friend} className="my-2">
+                    <a className="text-xs text-green-600" onClick={() => acceptInvitation(friend.id)}>
+                        accept invitation
+                    </a>
+                    </FriendInfo>
+                </li>
+                ))}
+            </ul>
+            : <div>Friendship requests will appear here</div>
+            }
+        </div>
       </div>
     </div>
   );
