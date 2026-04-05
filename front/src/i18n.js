9ff82c87a05f5c635e62b06c1e6f367a556a86f4
Irina Kalinina <irakalina2004@gmail.com>
2026-04-05 19:19:20 +0200

    i18n mainmenu

--- front/src/i18n.js
+++ front/src/i18n.js
@@ -9,21 +9,33 @@ const resources = {
     translation: {
       "login.title": "Login",
       "login.dont_have_account": "You don't have any account ?",
-      "login.forget_password": "You forget your password ?"
+      "login.forget_password": "You forget your password ?",
+      "main_menu.edit_profile": "Edit profile",
+      "main_menu.friends": "Friends",
+      "main_menu.settings": "Settings",
+      "main_menu.start_game": "Start Game"
     }
   },
   fr: {
     translation: {
       "login.title": "Connexion",
       "login.dont_have_account": "Vous n'avez pas de compte ?",
-      "login.forget_password": "Vous avez oublié votre mot de passe ?"
+      "login.forget_password": "Vous avez oublié votre mot de passe ?",
+      "main_menu.edit_profile": "Modifier le profil",
+      "main_menu.friends": "Amis",
+      "main_menu.settings": "Paramètres",
+      "main_menu.start_game": "Démarrer le jeu"
     }
   },
   ru: {
     translation: {
       "login.title": "Вход",
       "login.dont_have_account": "У вас нет аккаунта ?",
-      "login.forget_password": "Вы забыли пароль ?"
+      "login.forget_password": "Вы забыли пароль ?",
+      "main_menu.edit_profile": "Редактировать профиль",
+      "main_menu.friends": "Друзья",
+      "main_menu.settings": "Настройки",
+      "main_menu.start_game": "Начать игру"
     }
   }
 
