2de7e0f5682a14bc68b61ecb0f2a039c2c7a9520
sskobyak <scob@ya.ru>
2026-04-05 17:11:14 +0200

    menu

--- front/src/components/pages/Index/MainMenu.jsx
+++ front/src/components/pages/Index/MainMenu.jsx
@@ -7,8 +7,8 @@ export default function Index() {
         {/* <BigButton text="Main page" url="/" className="bg-red-500 hover:bg-red-700 border-red-700"/> */}
         <BigButton text="Edit profile" url="/editprofile" className="bg-blue-500 hover:bg-blue-700 border-blue-700"/>
         <BigButton text="Friends" url="/editfriends" className="bg-green-500 hover:bg-green-700 border-green-700"/>
-        <BigButton text="Game" url="/game" className="bg-purple-500 hover:bg-purple-700 border-purple-700"/>
         <BigButton text="Settings" url="/settings" className="bg-purple-500 hover:bg-purple-700 border-purple-700"/>
+        <BigButton text="Start Game" url="/game" className="bg-red-500 hover:bg-red-700 border-red-700"/>
     </div>
   );
 }
