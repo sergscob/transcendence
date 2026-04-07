302e0ef8463fcb2d34a7adc65274b2979e0edc9e
sskobyak <scob@ya.ru>
2026-04-07 22:56:56 +0200

    added game state at the bottom of the screen

--- front/src/pages/GameMain.tsx
+++ front/src/pages/GameMain.tsx
@@ -4,11 +4,26 @@ import { useUserStore } from "@/stores/userStore";
 import Loading from "../components/ui_int/Loading";
 import NotFound from "./NotFound";
 import { useTranslation } from 'react-i18next'
+import { IMatchState } from '@/game/roomState';  
+
+const defaultMatchState: IMatchState = {
+  current_player: {
+    user_id: 0,
+    health: 100,
+    arms_left: 5,
+    score: 0,
+  },
+  players_count: 1,
+  time_left: "00:00",
+};
 
 export default function GameMain() {
+
   const { t } = useTranslation();
   const containerRef = useRef<HTMLDivElement>(null)
   const [paused, setPaused] = useState(false)
+  const matchStateRef = useRef<IMatchState>(defaultMatchState)
+  const [matchState, setMatchState] = useState<IMatchState>(defaultMatchState)
   const user = useUserStore((s: any) => s.user);
   const loading = useUserStore((s: any) => s.loading);
   const loadUser = useUserStore((s: any) => s.loadUser);
@@ -17,11 +32,24 @@ export default function GameMain() {
     loadUser();
   }, [loadUser]);
 
+  const updateMatchState = (s: IMatchState) => {
+    const nextState: IMatchState = {
+      ...matchState,
+    }
+    matchStateRef.current = nextState
+    setMatchState(nextState)
+  }
+
   useEffect(() => {
     if (!containerRef.current || !user?.id)
       return
 
-    startGame(containerRef.current, user.id)
+    startGame(
+      containerRef.current,
+      user.id,
+      () => matchStateRef.current,
+      updateMatchState,
+    )
 
     const onPointerLockChange = () => {
       if (!document.pointerLockElement)
@@ -45,17 +73,31 @@ export default function GameMain() {
 
 
   return (
-    <div style={{ position: 'relative', width: '100%', height: '100%', overflow: 'hidden' }}>
+    <div className="relative w-screen h-screen overflow-hidden">
       <div ref={containerRef} style={{ width: '100%', height: '100%' }} />
       {paused && (
-        <div style={{
-          position: 'absolute',
-          display: 'flex', alignItems: 'center', justifyContent: 'center',
-          background: 'rgba(53, 47, 47, 0.5)', color: 'white', fontSize: '2rem'
-        }}>
+        <div className="absolute flex items-center justify-center bg-black/50 text-white text-2xl">
           {t("game_main.pause_click_resume")}
         </div>
       )}
+
+      <div className="absolute w-screen bottom-0 overflow-hidden bg-black/20 h-12 flex pt-2 ">
+        <div className={`font-display text-3xl ${matchState?.current_player.health < 30 ? "text-red-500/50" : "text-white/50"}`}>
+          HEALTH: {matchState?.current_player?.health}%
+        </div>
+        <div className={`ml-10 font-display text-3xl ${matchState?.current_player.arms_left < 2 ? "text-red-500/50" : "text-white/50"}`}>
+          ARMS: {matchState.current_player.arms_left}
+        </div>
+        <div className="ml-10 font-display  text-green-500/50 text-3xl">
+          SCORE: {matchState?.current_player?.score}
+        </div>
+        <div className="ml-10 font-display  text-yellow-500/50 text-3xl">
+          TIME LEFT: {matchState?.time_left}
+        </div>
+        <div className="ml-10 mr-10 font-display text-blue-500/50 text-3xl">
+          PLAYERS: {matchState?.players_count}
+        </div>
+      </div>
     </div>
   )
 }
