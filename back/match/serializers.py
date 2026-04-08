8f6e9573105cd390f528643dff5fc22728ad0e52
sskobyak <scob@ya.ru>
2026-04-08 18:46:11 +0200

    match fixes

--- back/match/serializers.py
+++ back/match/serializers.py
@@ -2,7 +2,11 @@
 from rest_framework import serializers
 from .models import Match
 
+
 class MatchSerializer(serializers.ModelSerializer):
+    num_players = serializers.IntegerField(read_only=True)
+    ready_players = serializers.IntegerField(read_only=True)
+
     class Meta:
         model = Match
         fields = "__all__"
