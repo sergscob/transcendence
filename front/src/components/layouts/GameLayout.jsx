889dc2c9b1533c84e3154d86b5504192333432ca
sskobyak <scob@ya.ru>
2026-04-09 19:23:45 +0200

    fix footer in GameLayout

--- front/src/components/layouts/GameLayout.jsx
+++ front/src/components/layouts/GameLayout.jsx
@@ -40,8 +40,6 @@ export default function MainLayout() {
         <Outlet />
       </div>
 
-      { location.pathname !== '/game' && <Footer /> } 
-
     </div>
   );
 }
--- front/src/components/layouts/MainLayout.jsx
+++ front/src/components/layouts/MainLayout.jsx
@@ -42,8 +42,6 @@ export default function MainLayout() {
         <Outlet />
       </div>
 
-      { location.pathname !== '/game' && <Footer /> } 
-
     </div>
   );
 }
