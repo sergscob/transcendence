9b69ba5366a463468bf30c7719ee83c4d1cd2574
sskobyak <scob@ya.ru>
2026-04-10 14:39:11 +0200

    back match gestion

--- back/sockets/game.py
+++ back/sockets/game.py
@@ -1,38 +1,17 @@
 import asyncio
 from typing import Any, Dict, TypedDict
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
+from channels.db import database_sync_to_async
+from .game_service import get_match_state, add_new_player, update_player, shot_user
 
-
-class PlayerState(TypedDict, total=False):
-    user_id: int
-    pos: list[float]
-    rockets: list[dict[str, Any]]
-
-class MatchState(TypedDict, total=False):
-    time: str
-    status: str
-    players: Dict[int, PlayerState]
-
-RoomStateByMatch = Dict[str, MatchState]
-
-ROOM_GAME_STATE_BY_MATCH: RoomStateByMatch = {}
 ROOM_LOCK: asyncio.Lock = asyncio.Lock()
 ROOM_BROADCAST_TASKS: Dict[str, asyncio.Task] = {}
 SEND_INTERVAL = 0.016
 
 
-def _new_match_state(match_id):
-    print (f"Creating new match state for match_id: {match_id}")
-    return {
-        'time': '0',
-        'status': 'waiting',
-        'players': {},
-    }
-
-
 async def broadcast_match_start(channel_layer, match_id: str, payload: Dict[str, Any] | None = None):
     event = {
-        'type': 'game_start',
+        'type': 'start',
         'payload': payload or {},
     }
     await channel_layer.group_send(f'game_{match_id}', event)
@@ -44,7 +23,10 @@ async def _broadcast_room_state(channel_layer, room_group_name: str, match_id: s
             await asyncio.sleep(SEND_INTERVAL)
 
             async with ROOM_LOCK:
-                match_state = ROOM_GAME_STATE_BY_MATCH.get(match_id, _new_match_state(match_id))
+                match_state = await get_match_state(match_id)
+
+            # if match_state is None or match_state.get('status') == 'waiting' :
+            #     continue
 
             await channel_layer.group_send(
                 room_group_name,
@@ -60,6 +42,7 @@ async def _broadcast_room_state(channel_layer, room_group_name: str, match_id: s
             task = ROOM_BROADCAST_TASKS.get(match_id)
             if task is asyncio.current_task():
                 ROOM_BROADCAST_TASKS.pop(match_id, None)
+
 
 
 class PlayerConsumer(AsyncJsonWebsocketConsumer):
@@ -78,7 +61,6 @@ class PlayerConsumer(AsyncJsonWebsocketConsumer):
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
 
         async with ROOM_LOCK:
-            ROOM_GAME_STATE_BY_MATCH.setdefault(self.match_id, _new_match_state(self.match_id))
             task = ROOM_BROADCAST_TASKS.get(self.match_id)
             if task is None or task.done():
                 ROOM_BROADCAST_TASKS[self.match_id] = asyncio.create_task(
@@ -93,13 +75,13 @@ class PlayerConsumer(AsyncJsonWebsocketConsumer):
         task_to_cancel = None
 
         async with ROOM_LOCK:
-            room_state = ROOM_GAME_STATE_BY_MATCH.get(self.match_id)
+            room_state = await get_match_state(self.match_id)
             if room_state is not None and self.user_id is not None:
                 players = room_state.setdefault('players', {})
                 players.pop(self.user_id, None)
 
             if room_state is not None and not room_state.get('players'):
-                ROOM_GAME_STATE_BY_MATCH.pop(self.match_id, None)
+                disconnect_player(self.match_id, self.user_id)
                 task_to_cancel = ROOM_BROADCAST_TASKS.pop(self.match_id, None)
                 should_cancel_task = task_to_cancel is not None
 
@@ -110,9 +92,8 @@ class PlayerConsumer(AsyncJsonWebsocketConsumer):
 
     async def receive_json(self, content, **kwargs):
         message_type = content.get('type', 'state')
-        room_match_id = content.get('match_id')
         user_id = content.get('user_id')
-        if user_id is None or str(room_match_id) != self.match_id:
+        if user_id is None :
             return
         self.user_id = user_id
 
@@ -120,13 +101,23 @@ class PlayerConsumer(AsyncJsonWebsocketConsumer):
             state = content.get('state')
             await self.update_game_state(user_id, state)
             return
-        
+
+        if message_type == 'shot':
+            user_id = content.get('user_id')
+            shot_id = content.get('shot_id')
+            damage = content.get('damage', 10)
+            score = content.get('score', 10)
+            await shot_user(self.match_id, user_id, shot_id, damage, score)
+            return
+
+
         print(f"Received unknown message from user {user_id} in match {self.match_id}: {content}")
+
 
     async def room_state(self, event):
         await self.send_json({
             'type': 'state',
-            'matchState': event.get('match_state', _new_match_state(self.match_id)),
+            'matchState': event.get('match_state'),
         })
 
     async def game_start(self, event):
@@ -135,20 +126,9 @@ class PlayerConsumer(AsyncJsonWebsocketConsumer):
             **event.get('payload', {}),
         })
 
-    # async def send_room_state(self):
-    #     async with ROOM_LOCK:
-    #         states = list(ROOM_GAME_STATE_BY_MATCH.get(self.match_id, {}).values())
-    #     await self.send_json({
-    #         'type': 'state',
-    #         'states': states,
-    #     })
-
     async def update_game_state(self, user_id: int, state: Any):
         async with ROOM_LOCK:
             if isinstance(state, dict):
-                merged = dict(state)
-                merged['user_id'] = user_id
-                match_state = ROOM_GAME_STATE_BY_MATCH.setdefault(self.match_id, _new_match_state(self.match_id))
-                players = match_state.setdefault('players', {})
-                players[user_id] = merged
-                return
+                userInfo = dict(state)
+                userInfo['user_id'] = user_id
+                await update_player(self.match_id, userInfo)
