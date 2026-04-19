2aa51dcf5215d929522359b0dc3aae32988cc7bd
sskobyak <scob@ya.ru>
2026-04-19 21:58:00 +0200

    sort statistics

--- front/src/pages/TotalStat.jsx
+++ front/src/pages/TotalStat.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { useNavigate } from 'react-router-dom';
 import API from "@/api/api";
 import { useUserStore } from "@/stores/userStore";
 import Loading from "@/components/ui_int/Loading";
@@ -7,7 +8,7 @@ import NotFound from "@/pages/NotFound";
 import { useTranslation } from "react-i18next";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
 import { Link } from "react-router-dom";
-
+import ButtonClose from "@/components/ui_int/ButtonClose";
 
 function TotalStat() {
     const { t } = useTranslation();
@@ -16,12 +17,35 @@ function TotalStat() {
     const loading = useUserStore((s) => s.loading);
     const [statData, setStatData] = useState([]);
     const [statArray, setStatArray] = useState([]);
+    const [sortBy, setSortBy] = useState("place");
+    const navigate = useNavigate();
+
+    function showSorted(sort) {
+        let sort2 = sort[0] === "-" ? sort.slice(1) : sort;
+        let sortBy2 = sortBy[0] === "-" ? sortBy.slice(1) : sortBy;
+        if (sort2 === sortBy2) {
+            if (sortBy[0] === '-') 
+                setSortBy(sort2);
+            else 
+                setSortBy("-" + sort2);
+        } else {
+            setSortBy(sort);
+        }
+    }
+
+    function getSortIcon(sort) {
+        let sortBy2 = sortBy[0] === "-" ? sortBy.slice(1) : sortBy;
+        if (sort === sortBy2) 
+            return sortBy[0] === '-' ? "↓" : "↑";
+        return "";
+    }   
 
     async function fetchAll() {
-        const res = await API.get("stats/")
+        const res = await API.get("stats/?order=" + sortBy)
         setStatData(res.data);
         setStatArray(res.data.results);
     }
+
     async function fetchNext() {
         if (!statData.next)
             return;
@@ -32,7 +56,7 @@ function TotalStat() {
 
     useEffect(() => {
         fetchAll();
-    }, []);
+    }, [sortBy]);
 
 
     if (loading) return <Loading />;
@@ -40,19 +64,29 @@ function TotalStat() {
 
     return (
         <div className="w-screen h-screen flex justify-center items-center">
-
-            <div className="flex flex-col gap-4 border border-black rounded-md p-4 shadow-lg bg-gray-500 min-w-150" >
+            <div className="flex flex-col gap-4 border border-black rounded-md p-4 shadow-lg bg-gray-500 min-w-150 relative" >
+                <ButtonClose onClose={() => navigate(-1)} className="absolute top-4 right-4" /> 
 
                 <div className="text-[20px] text-white">{t("total_stat.title")}</div>
 
                 <Table className="text-white">
                     <TableHeader>
                         <TableRow>
-                            <TableHead>{t("total_stat.place")}</TableHead>
-                            <TableHead className="w-25">{t("total_stat.player")}</TableHead>
-                            <TableHead>{t("total_stat.total_matches")}</TableHead>
-                            <TableHead>{t("total_stat.wins")}</TableHead>
-                            <TableHead>{t("total_stat.score")}</TableHead>
+                            <TableHead className="cursor-pointer" onClick={() => showSorted("place")}>
+                                {t("total_stat.place")} {getSortIcon("place")}
+                            </TableHead>
+                            <TableHead className="w-25 cursor-pointer" onClick={() => showSorted("username")}>
+                                {t("total_stat.player")} {getSortIcon("username")}
+                            </TableHead>
+                            <TableHead className="cursor-pointer" onClick={() => showSorted("total_matches")}>
+                                {t("total_stat.total_matches")} {getSortIcon("total_matches")}
+                            </TableHead>
+                            <TableHead className="cursor-pointer" onClick={() => showSorted("wins")}>
+                                {t("total_stat.wins")} {getSortIcon("wins")}
+                            </TableHead>
+                            <TableHead className="cursor-pointer" onClick={() => showSorted("score")}>
+                                {t("total_stat.score")} {getSortIcon("score")}
+                            </TableHead>
                         </TableRow>
                     </TableHeader>
                     <TableBody>
@@ -72,7 +106,6 @@ function TotalStat() {
                         onClick={() => fetchNext()}
                     >
                         {t("total_stat.load_more")}
-
                     </Button>
                 )}
             </div>
