c04a60c86b879cd8969324d2236c3ce25367302a
sskobyak <scob@ya.ru>
2026-04-27 14:51:50 +0200

    delete strictmode

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -5,7 +5,7 @@ import './i18n';
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  // <StrictMode>
     <App />
-  </StrictMode>,
+  // </StrictMode>,
 )
