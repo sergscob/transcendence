a5d897626ce51c01fe92d3cfbdee9796802b029b
sskobyak <scob@ya.ru>
2026-05-06 16:50:21 +0200

    https-nginx

--- front/src/stores/settingsStore.ts
+++ front/src/stores/settingsStore.ts
@@ -5,7 +5,6 @@ type LanguagesEnum = "en" | "fr" | "ru";
 
 type SettingsState = {
     serverIP: string;
-    htppsServerIP: string;
     language: LanguagesEnum;
     setServerIp: (serverIp: string) => void;
     setLanguage: (language: LanguagesEnum) => void;
@@ -17,7 +16,6 @@ export const useSettingsStore = create<SettingsState>()(
     persist(
         (set, get) => ({
             serverIP: "localhost:8000",
-            htppsServerIP: "localhost",
             language: "en",
 
             setServerIp: (serverIp: string) =>set((state: SettingsState) => ({
@@ -28,25 +26,39 @@ export const useSettingsStore = create<SettingsState>()(
                 language,
             })),
 
+            // getServerHttpUrl: () => {
+            //     console.log("Getting server HTTP URL", import.meta.env.VITE_USE_HTTPS);
+            //     const serverIP = get().serverIP;
+            //     if (serverIP.startsWith("http://") || serverIP.startsWith("https://")) {
+            //         return serverIP;
+            //     }
+            //     let pageProtocol = typeof window !== "undefined" && window.location.protocol === "https:" ? "https" : "http";
+            //     console.log(pageProtocol)
+            //     pageProtocol = 'https:'
+            //     // return `${pageProtocol}://${pageProtocol === 'https' ? get().htppsServerIP : serverIP}`;
+            //     return "https://localhost:8000"
+            // },
+            // getServerWsUrl: () => {
+            //     const serverIP = get().serverIP;
+            //     if (serverIP.startsWith("http://")) {
+            //         return "ws://" + serverIP.slice(7);
+            //     } else if (serverIP.startsWith("https://")) {
+            //         return "wss://" + serverIP.slice(8);
+            //     }
+            //     const wsProtocol = typeof window !== "undefined" && window.location.protocol === "https:" ? "wss" : "ws";
+            //     return 'wss://localhost:8000'
+            //     return `${wsProtocol}://${wsProtocol === 'wss' ? get().htppsServerIP : serverIP}`;
+            // }
             getServerHttpUrl: () => {
                 const serverIP = get().serverIP;
-                if (serverIP.startsWith("http://") || serverIP.startsWith("https://")) {
-                    return serverIP;
-                }
-                const pageProtocol = typeof window !== "undefined" && window.location.protocol === "https:" ? "https" : "http";
-                console.log(pageProtocol)
-                return `${pageProtocol}://${pageProtocol === 'https' ? get().htppsServerIP : serverIP}`;
+                console.log("Getting server HTTP URL", import.meta.env.VITE_USE_HTTPS, import.meta.env.VITE_USE_HTTPS ? `https://${serverIP}` : `https://${serverIP}`);
+                return import.meta.env.VITE_USE_HTTPS ? `https://${serverIP}` : `https://${serverIP}`
             },
             getServerWsUrl: () => {
                 const serverIP = get().serverIP;
-                if (serverIP.startsWith("http://")) {
-                    return "ws://" + serverIP.slice(7);
-                } else if (serverIP.startsWith("https://")) {
-                    return "wss://" + serverIP.slice(8);
-                }
-                const wsProtocol = typeof window !== "undefined" && window.location.protocol === "https:" ? "wss" : "ws";
-                return `${wsProtocol}://${wsProtocol === 'wss' ? get().htppsServerIP : serverIP}`;
+                return import.meta.env.VITE_USE_HTTPS ? `wss://${serverIP}` : `ws://${serverIP}`
             }
+
         }),
         { name: "trans-settings-storage" },
     )
